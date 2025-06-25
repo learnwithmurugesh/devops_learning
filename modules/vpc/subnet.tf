ce764d58194f36711c6b26040afa9ce410a7aef0
learnwithmurugesh <learnwithmurugesh@gmail.com>
2025-06-25 19:56:05 +0530

    subnet file updated

--- modules/vpc/subnet.tf
+++ modules/vpc/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
-  vpc_id                  = aws_vpc.this.id
+  vpc_id                  = aws_vpc.audi.id
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
